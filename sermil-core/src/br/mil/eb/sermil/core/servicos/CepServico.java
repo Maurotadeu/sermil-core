d3099211d1c212a9122ef2b31d4191f462cd4358
Wagner Lopes <wagner.luis.alopes@gmail.com>
2015-05-08 10:29:53 -0300

    Adicionado CepServico.java para buscar CEP

---
+++ sermil-core/src/br/mil/eb/sermil/core/servicos/CepServico.java
@@ -0,0 +1,38 @@
+package br.mil.eb.sermil.core.servicos;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.directwebremoting.annotations.RemoteMethod;
+import org.directwebremoting.annotations.RemoteProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.mil.eb.sermil.core.dao.CepDao;
+import br.mil.eb.sermil.core.exceptions.SermilException;
+import br.mil.eb.sermil.modelo.Cep;
+
+/** Serviço de informações de CEP. (View CEP)
+ * @author Abreu Lopes
+ * @since 5.1
+ * @version $Id$
+ */
+@Named("cepServico")
+@RemoteProxy(name="cepServico")
+public class CepServico {
+
+  protected static final Logger logger = LoggerFactory.getLogger(CepServico.class);
+
+  @Inject
+  private CepDao cepDao;
+
+  public CepServico() {
+    logger.debug("CepServico iniciado");
+  }
+
+  @RemoteMethod
+  public Cep recuperar(final String codigo) throws SermilException {
+    return this.cepDao.findById(codigo);
+  }
+
+}
