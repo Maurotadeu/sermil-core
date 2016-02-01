97ff1cfed65e806312b4d7d063bd091938f5cc0c
Wagner Lopes <wagner.luis.alopes@gmail.com>
2016-02-01 17:11:35 -0200

    Modelagem de Agendamento de CS

--- sermil-core/src/main/java/br/mil/eb/sermil/core/dao/CsAgendamentoDao.java
+++ sermil-core/src/main/java/br/mil/eb/sermil/core/dao/CsAgendamentoDao.java
@@ -2,6 +2,6 @@ package br.mil.eb.sermil.core.dao;
 
 import br.mil.eb.sermil.modelo.CsAgendamento;
 
-public interface CsAgendamentoDao extends GenericDao<CsAgendamento, Integer> {
+public interface CsAgendamentoDao extends GenericDao<CsAgendamento, CsAgendamento.PK> {
   
 }
