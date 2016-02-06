98726e6268d7e17bd0bcca53e0f440ebc471957e
Anselmo S Ribeiro <6975.usa@gmail.com>
2016-02-06 11:05:12 -0200

    Criar testes com Selenium

--- sermil-core/src/test/java/selenium/admin/commons/AdminTestSuite.java
+++ sermil-core/src/test/java/selenium/admin/commons/AdminTestSuite.java
@@ -3,8 +3,10 @@ package selenium.admin.commons;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import selenium.SermilTest;
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses({})
-public class AdminTestSuite {
+public class AdminTestSuite extends SermilTest{
 
 }
