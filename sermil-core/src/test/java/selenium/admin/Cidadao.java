6e3f49e8dac680343db490b97ae9f40a48f96bd7
Anselmo S Ribeiro <6975.usa@gmail.com>
2016-02-09 09:28:21 -0200

    Mover e renomear classe de teste de cidadao

---
+++ sermil-core/src/test/java/selenium/admin/Cidadao.java
@@ -0,0 +1,22 @@
+package selenium.admin;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Cidadao {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public final void testRecuperarLong() {
+		// TODO
+	}
+
+}
