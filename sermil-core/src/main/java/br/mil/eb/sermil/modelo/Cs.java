affc30d406d5c319a935c4fe7e6bda14f9f9bfac
Wagner Lopes <wagner.luis.alopes@gmail.com>
2016-07-28 18:51:41 -0300

    Correção da validação de CS

--- sermil-core/src/main/java/br/mil/eb/sermil/modelo/Cs.java
+++ sermil-core/src/main/java/br/mil/eb/sermil/modelo/Cs.java
@@ -41,163 +41,157 @@ import br.mil.eb.sermil.core.exceptions.SermilException;
 @PrimaryKey(validation=IdValidation.NULL)
 public final class Cs implements Serializable {
 
-   private static final long serialVersionUID = 8846493879279340834L;
+  private static final long serialVersionUID = 8846493879279340834L;
 
-   @Id
-   @GeneratedValue(strategy=GenerationType.TABLE, generator="CS")
-   @TableGenerator(name="CS", allocationSize=1)
-   private Integer codigo;
+  @Id
+  @GeneratedValue(strategy=GenerationType.TABLE, generator="CS")
+  @TableGenerator(name="CS", allocationSize=1)
+  private Integer codigo;
 
-   @Column
-   private Integer atende;
+  @Column
+  private Integer atende;
 
-   @Column
-   private String nome;
+  @Column
+  private String nome;
 
-   @Column
-   private Integer numero;
+  @Column
+  private Integer numero;
 
-   @ManyToOne(cascade=CascadeType.REFRESH)
-   @JoinColumn(name = "RM_CODIGO", referencedColumnName = "CODIGO", nullable = false)
-   private Rm rm;
+  @ManyToOne(cascade=CascadeType.REFRESH)
+  @JoinColumn(name = "RM_CODIGO", referencedColumnName = "CODIGO", nullable = false)
+  private Rm rm;
 
-   @OneToMany(mappedBy = "cs", fetch = FetchType.EAGER, cascade = CascadeType.ALL , orphanRemoval = true)
-   @JoinColumn(name = "CS_CODIGO", referencedColumnName = "CODIGO")
-   private List<CsExclusaoData> csExclusaoDataCollection;
+  @OneToMany(mappedBy = "cs", fetch = FetchType.EAGER, cascade = CascadeType.ALL , orphanRemoval = true)
+  @JoinColumn(name = "CS_CODIGO", referencedColumnName = "CODIGO")
+  private List<CsExclusaoData> csExclusaoDataCollection;
 
-   @OneToMany(mappedBy = "cs", fetch = FetchType.EAGER, cascade = CascadeType.ALL , orphanRemoval = true)
-   @JoinColumn(name = "CS_CODIGO", referencedColumnName = "CODIGO")
-   private List<CsFuncionamento> csFuncionamentoCollection;
+  @OneToMany(mappedBy = "cs", fetch = FetchType.EAGER, cascade = CascadeType.ALL , orphanRemoval = true)
+  @JoinColumn(name = "CS_CODIGO", referencedColumnName = "CODIGO")
+  private List<CsFuncionamento> csFuncionamentoCollection;
 
-   public Cs() {
-      super();
-   }
+  public Cs() {
+    super();
+  }
 
-   @Override
-   public String toString() {
-      return new StringBuilder("CS ").append(new DecimalFormat("000").format(this.getNumero()))
-            .append("/").append(new DecimalFormat("00").format(this.getRm().getCodigo())).toString();
-   }
-   
-   @Override
-   public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((numero == null) ? 0 : numero.hashCode());
-      result = prime * result + ((rm == null) ? 0 : rm.hashCode());
-      return result;
-   }
+  @Override
+  public String toString() {
+    return new StringBuilder("CS ").append(new DecimalFormat("000").format(this.getNumero()))
+        .append("/").append(new DecimalFormat("00").format(this.getRm().getCodigo())).toString();
+  }
 
-   @Override
-   public boolean equals(Object obj) {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      Cs other = (Cs) obj;
-      if (numero == null) {
-         if (other.numero != null)
-            return false;
-      } else if (!numero.equals(other.numero))
-         return false;
-      if (rm == null) {
-         if (other.rm != null)
-            return false;
-      } else if (!rm.equals(other.rm))
-         return false;
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
       return true;
-   }
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Cs other = (Cs) obj;
+    if (codigo == null) {
+      if (other.codigo != null)
+        return false;
+    } else if (!codigo.equals(other.codigo))
+      return false;
+    return true;
+  }
 
-   public Integer getCodigo() {
-      return codigo;
-   }
+  public Integer getCodigo() {
+    return codigo;
+  }
 
-   public void setCodigo(Integer codigo) {
-      this.codigo = codigo;
-   }
+  public void setCodigo(Integer codigo) {
+    this.codigo = codigo;
+  }
 
-   public Rm getRm() {
-      return rm;
-   }
+  public Rm getRm() {
+    return rm;
+  }
 
-   public void setRm(Rm rm) {
-      this.rm = rm;
-   }
+  public void setRm(Rm rm) {
+    this.rm = rm;
+  }
 
-   public String getNome() {
-      return nome;
-   }
+  public String getNome() {
+    return nome;
+  }
 
-   public void setNome(String nome) {
-      this.nome = nome;
-   }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-   public Integer getAtende() {
-      return atende;
-   }
+  public Integer getAtende() {
+    return atende;
+  }
 
-   public void setAtende(Integer atende) {
-      this.atende = atende;
-   }
+  public void setAtende(Integer atende) {
+    this.atende = atende;
+  }
 
-   public Integer getNumero() {
-      return numero;
-   }
+  public Integer getNumero() {
+    return numero;
+  }
 
-   public void setNumero(Integer numero) {
-      this.numero = numero;
-   }
-   
-   public List<CsExclusaoData> getCsExclusaoDataCollection() {
-      return csExclusaoDataCollection;
-   }
+  public void setNumero(Integer numero) {
+    this.numero = numero;
+  }
 
-   public void setCsExclusaoDataCollection(List<CsExclusaoData> csExclusaoDataCollection) {
-      this.csExclusaoDataCollection = csExclusaoDataCollection;
-   }
+  public List<CsExclusaoData> getCsExclusaoDataCollection() {
+    return csExclusaoDataCollection;
+  }
 
-   public void addCsExclusaoData(final CsExclusaoData csExclusaoData) throws SermilException {
-      if (this.csExclusaoDataCollection == null) {
-         this.csExclusaoDataCollection = new ArrayList<CsExclusaoData>();
-      }
-      if (this.csExclusaoDataCollection.contains(csExclusaoData)) {
-         throw new SermilException("Data de Exclusão já existe");
-      }
-      this.csExclusaoDataCollection.add(csExclusaoData);
-      if (csExclusaoData.getCs() != this) {
-         csExclusaoData.setCs(this);
-      }
-   }
-   
-   public List<CsFuncionamento> getCsFuncionamentoCollection() {
-      return csFuncionamentoCollection;
-   }
+  public void setCsExclusaoDataCollection(List<CsExclusaoData> csExclusaoDataCollection) {
+    this.csExclusaoDataCollection = csExclusaoDataCollection;
+  }
 
-   public void setCsFuncionamentoCollection(List<CsFuncionamento> csFuncionamentoCollection) {
-      this.csFuncionamentoCollection = csFuncionamentoCollection;
-   }
-   
-   public void addCsFuncionamento(CsFuncionamento csFuncionamento) throws SermilException {
-      if (this.csFuncionamentoCollection == null) {
-         this.csFuncionamentoCollection = new ArrayList<CsFuncionamento>();
-      }
-      if (this.csFuncionamentoCollection.contains(csFuncionamento)) {
-         throw new SermilException("Funcionamento de CS já existe");
-      }
-      this.csFuncionamentoCollection.add(csFuncionamento);
-      if (csFuncionamento.getCs() != this) {
-         csFuncionamento.setCs(this);
-      }
-   }
+  public void addCsExclusaoData(final CsExclusaoData csExclusaoData) throws SermilException {
+    if (this.csExclusaoDataCollection == null) {
+      this.csExclusaoDataCollection = new ArrayList<CsExclusaoData>();
+    }
+    if (this.csExclusaoDataCollection.contains(csExclusaoData)) {
+      throw new SermilException("Data de Exclusão já existe");
+    }
+    this.csExclusaoDataCollection.add(csExclusaoData);
+    if (csExclusaoData.getCs() != this) {
+      csExclusaoData.setCs(this);
+    }
+  }
 
-   public void removeCsFuncionamento(CsFuncionamento csFuncionamento) throws SermilException {
-      if (!this.csFuncionamentoCollection.contains(csFuncionamento)) {
-         throw new SermilException("Funcionamento de CS não existe");
-      }
-      this.csFuncionamentoCollection.remove(csFuncionamento);
-      csFuncionamento.setCs(null);
-   }
+  public List<CsFuncionamento> getCsFuncionamentoCollection() {
+    return csFuncionamentoCollection;
+  }
+
+  public void setCsFuncionamentoCollection(List<CsFuncionamento> csFuncionamentoCollection) {
+    this.csFuncionamentoCollection = csFuncionamentoCollection;
+  }
+
+  public void addCsFuncionamento(CsFuncionamento csFuncionamento) throws SermilException {
+    if (this.csFuncionamentoCollection == null) {
+      this.csFuncionamentoCollection = new ArrayList<CsFuncionamento>();
+    }
+    if (this.csFuncionamentoCollection.contains(csFuncionamento)) {
+      throw new SermilException("Funcionamento de CS já existe");
+    }
+    this.csFuncionamentoCollection.add(csFuncionamento);
+    if (csFuncionamento.getCs() != this) {
+      csFuncionamento.setCs(this);
+    }
+  }
+
+  public void removeCsFuncionamento(CsFuncionamento csFuncionamento) throws SermilException {
+    if (!this.csFuncionamentoCollection.contains(csFuncionamento)) {
+      throw new SermilException("Funcionamento de CS não existe");
+    }
+    this.csFuncionamentoCollection.remove(csFuncionamento);
+    csFuncionamento.setCs(null);
+  }
 
 }
